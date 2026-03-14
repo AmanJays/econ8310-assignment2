af0a54ec6d2b55e8fff89371f5e7fc4ef063b26f
AmanJays <amanjays42@gmail.com>
2026-03-14 10:37:18 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,38 +1,38 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-# Load training data (LOCAL FILE)
-train = pd.read_csv("assignment2train.csv")
+# Load training data
+train = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 
-# Feature engineering
+# Convert datetime
 train["DateTime"] = pd.to_datetime(train["DateTime"])
 train["hour"] = train["DateTime"].dt.hour
 train["day"] = train["DateTime"].dt.dayofweek
 
 # Drop unused columns
-train = train.drop(columns=["id","DateTime"])
+train = train.drop(columns=["id", "DateTime"])
 
-# Features and target
+# Split features and target
 X = train.drop(columns=["meal"])
 y = train["meal"]
 
-# Model
-model = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=1)
+# Define model
+model = RandomForestClassifier(n_estimators=200, random_state=42)
 
 # Fit model
 modelFit = model.fit(X, y)
 
-# Load test data (LOCAL FILE)
-test = pd.read_csv("assignment2test.csv")
+# Load test data
+test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
 
 # Same feature engineering
 test["DateTime"] = pd.to_datetime(test["DateTime"])
 test["hour"] = test["DateTime"].dt.hour
 test["day"] = test["DateTime"].dt.dayofweek
 
-test = test.drop(columns=["id","DateTime"])
+test = test.drop(columns=["id", "DateTime"])
 
-# Predictions
+# Generate predictions
 pred = modelFit.predict(test)
 
 # Convert to list of integers
