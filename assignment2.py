47b366c9654da8a1139b9fc38553afd4042c2b06
AmanJays <amanjays42@gmail.com>
2026-03-14 12:16:06 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,24 +1,42 @@
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 
-train_data = pd.read_csv('assignment2train.csv')
-X = train_data.drop(columns=['meal', 'id'], errors='ignore')
-y = train_data['meal']
-X = pd.get_dummies(X)
+# URLs for training and test data
+train_url = "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv"
+test_url = "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv"
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+# Load data
+train_data = pd.read_csv(train_url)
+test_data = pd.read_csv(test_url)
 
-model = RandomForestClassifier(n_estimators=100, max_depth=10, max_features='sqrt', random_state=1, n_jobs=1)
+# Optional: create simple time-based features if timestamp exists
+for df in [train_data, test_data]:
+    if 'timestamp' in df.columns:
+        df['hour'] = pd.to_datetime(df['timestamp']).dt.hour
+        df['day_of_week'] = pd.to_datetime(df['timestamp']).dt.dayofweek
+        df.drop(columns=['timestamp'], inplace=True)
+
+# Drop irrelevant columns if they exist
+train_data = train_data.drop(columns=['id'], errors='ignore')
+test_features = test_data.drop(columns=['id'], errors='ignore')
+
+# Separate target
+X_train = train_data.drop(columns=['meal'], errors='ignore')
+y_train = train_data['meal']
+
+# Convert categorical columns to dummies
+X_train = pd.get_dummies(X_train)
+test_features = pd.get_dummies(test_features)
+test_features = test_features.reindex(columns=X_train.columns, fill_value=0)
+
+# Fit RandomForest
+model = RandomForestClassifier(n_estimators=100, max_depth=6, random_state=42)
 modelFit = model.fit(X_train, y_train)
 
-pred = [int(x) for x in modelFit.predict(X_test)]
-print(pred[:20])
+# Predict on test set (Python ints)
+pred = [int(x) for x in modelFit.predict(test_features)]
 
-test_data = pd.read_csv('assignment2test.csv')
-test_data = test_data.drop(columns=['id'], errors='ignore')
-test_data = pd.get_dummies(test_data)
-test_data = test_data.reindex(columns=X_train.columns, fill_value=0)
-
-pred_test = [int(x) for x in modelFit.predict(test_data)]
-print(pred_test[:20])
+# Output for sanity check
+print("Number of predictions:", len(pred))
+print("Sample predictions:", pred[:20])
